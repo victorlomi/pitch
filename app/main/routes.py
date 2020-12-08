1e0953b538b459df00100148d5111c62ee2f17b2
plutos28 <gamevictor28@gmail.com>
2020-12-08 10:51:40 +0300

    add login route with finished login template

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,12 @@
+from flask import render_template
 from app.main import bp
+from app.main import forms
 
 @bp.route('/')
 def index():
     return 'welcome to pitch!'
+
+@bp.route('/login')
+def login():
+    form = forms.LoginForm()
+    return render_template('login.html', form=form)

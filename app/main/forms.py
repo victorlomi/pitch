7693f39f791d6d5616a57382b4dd0a8dfbedf59e
victorlomi <victorlominyo@gmail.com>
2020-12-08 16:51:27 +0300

    change wording from register to signup

--- app/main/forms.py
+++ app/main/forms.py
@@ -14,7 +14,7 @@ class SignupForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField("Register")
+    submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()

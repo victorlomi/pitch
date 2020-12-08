ee5a996615e5bb004b895a8cd648d34286b880de
plutos28 <gamevictor28@gmail.com>
2020-12-08 12:07:54 +0300

    handle form validation and flash form information after

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.main import bp
 from app.main import forms
 
@@ -6,7 +6,16 @@ from app.main import forms
 def index():
     return render_template('index.html') 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
+    # create instance of login form to be passed in to template
     form = forms.LoginForm()
+
+    # handle post response from form
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+
+        # redirect to home - TODO - change to posts route
+        return redirect('/') 
+
     return render_template('login.html', form=form)

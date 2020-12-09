bd70c774fb359c960e6d95be65d29e11876d7c52
victorlomi <victorlominyo@gmail.com>
2020-12-09 07:14:40 +0300

    add logic for adding pitch into database

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user
 from app.main import bp
 from app import db
@@ -15,12 +15,18 @@ def category(category):
     pitches = Pitch().query.filter_by(category=category).all()
     return render_template('category.html', category=category, pitches=pitches)
 
-@bp.route('/add-pitch')
+@bp.route('/add-pitch', methods=['GET', 'POST'])
 def add_pitch():
     # go to login in page if user is not logged in
     if current_user.is_anonymous:
         return redirect(url_for('auth.login'))
 
     form = forms.AddForm()
+
+    if form.validate_on_submit():
+        pitch = Pitch(author=current_user, body=form.body.data, category=form.category.data)
+        db.session.add(pitch)
+        db.session.commit()
+        return redirect(url_for('main.index')) 
     
     return render_template('add_pitch.html', form=form)

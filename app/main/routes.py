3c1ea3b2585f7aeb3d24f099e604766fda402faa
plutos28 <gamevictor28@gmail.com>
2020-12-09 04:34:18 +0300

    add route to add pitch page

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,5 +10,9 @@ def index():
 @bp.route('/category/<category>')
 def category(category):
     # retrieve all pitches in category
-    pitches = Pitch().query.all()
-    return render_template('category.html', category=category)
+    pitches = Pitch().query.filter_by(category=category).all()
+    return render_template('category.html', category=category, pitches=pitches)
+
+@bp.route('/add-pitch')
+def add_pitch():
+    return render_template('add_pitch.html')

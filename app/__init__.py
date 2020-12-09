8b29b3a7e8f1030cbb3b41458b55f32ad1a8c525
victorlomi <victorlominyo@gmail.com>
2020-12-09 05:52:17 +0300

    add flask-bootstrap and style navbar

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+bootstrap = Bootstrap()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
+    bootstrap.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
